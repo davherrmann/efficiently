843a3b61b5ec279962693984f4ac9b0c053aac02
David Herrmann <davherrmann@googlemail.com>
2016-03-31 17:50:43 +0200

    import actions explicitly

--- app/app.jsx
+++ app/app.jsx
@@ -11,18 +11,18 @@ import Test from './test';
 import component from '../framework/component';
 
 // actions
-import * as actions from '../actions';
+import {server, submit, dialogAction} from '../actions';
 
 export default component((state, dispatch) => (
   <Ewb actions={state.ewb.actions} title={state.ewb.title} onSubmit={() => this.refs.form.submit()}>
     <h1>Test</h1>
     <Form
       ref="form"
-      onSubmit={data => dispatch(actions.server(actions.submit()))}>
+      onSubmit={data => dispatch(server(submit()))}>
     </Form>
     <Dialog
       title="Super major feedback question"
-      actions={[actions.dialogAction('reallyClose', 'Yes, CLOSE the thing!')]}
+      actions={[dialogAction('reallyClose', 'Yes, CLOSE the thing!')]}
       hidden={!state.wantToClose}>
       Do you really want to close?
     </Dialog>
