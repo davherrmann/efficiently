995ea61378d0c196f1c3dc860b7094f17b49477d
David Herrmann <davherrmann@googlemail.com>
2016-06-03 06:07:49 +0200

    use seamless-immutable merging capabilities

--- src/reducers/applyNewState.js
+++ src/reducers/applyNewState.js
@@ -1,12 +1,11 @@
 import Immutable from 'seamless-immutable';
-import Merger from '../core/merger';
 
 export default (state = Immutable({}), action) => {
   switch (action.type) {
     case 'applyStateFromServer':
-      let merged0 = new Merger().merge(state.asMutable({deep: true}), action.state);
-      let merged = Immutable(merged0).set('ready', true);
-      return merged;
+      return Immutable(state)
+        .merge(action.state, {deep: true})
+        .set('ready', true);
     default:
       return state;
   }
